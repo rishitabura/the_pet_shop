cf693c345905f771b0c4644ce0455e597e5286e7
Gauri-Choudhari <rcgauri1412@gmail.com>
2023-08-21 17:59:38 +0530

    Routes

--- views/client/src/App.js
+++ views/client/src/App.js
@@ -1,25 +1,29 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+//import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/home";
 import About from './pages/aboutus';
 
+import Login from './pages/login';
+import Register from './pages/register';
 
-<<<<<<< HEAD
-=======
 import Animals from './pages/animals';
 
->>>>>>> 524bffb5ec26051e2233e7055ec5d1044c0c23c3
+
 function App() {
   return (
     <>
      <BrowserRouter>
      <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+
+          <Route exact path="/animals" element={<Animals />} />
       </Routes>
       </BrowserRouter>
     </>
