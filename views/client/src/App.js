0ece2dd1d88aaf7b386a08f56d46181e67f2f358
Rishita <burarishita@gmail.com>
2023-08-21 16:21:42 +0530

    changes in app.js

--- views/client/src/App.js
+++ views/client/src/App.js
@@ -1,43 +1,43 @@
 // import logo from './logo.svg';
 import './App.css';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/home";
 import About from './pages/aboutus';
 
 
-// function App() {
-//   return (
-//     <>
-//      <BrowserRouter>
-//      <Routes>
-//           <Route exact path="/" element={<Home />} />
-//       </Routes>
-//       </BrowserRouter>
-//     </>
-//   );
-// }
-
 function App() {
   return (
     <>
-     <Router>
-       <Switch>
-
+     <BrowserRouter>
+     <Routes>
           <Route exact path="/" element={<Home />} />
-
-          <Route path="/about" component={About} />
-
-          <Redirect to="/" />
-
-      </Switch>
-      </Router>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
+
+// function App() {
+//   return (
+//     <>
+//      <Router>
+//        <Switch>
+
+//           <Route exact path="/" element={<Home />} />
+
+//           <Route path="/about" component={About} />
+
+//           <Redirect to="/" />
+
+//       </Switch>
+//       </Router>
+//     </>
+//   );
+// }
 
 export default App;
 
