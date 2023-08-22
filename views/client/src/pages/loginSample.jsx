2b4ecf47aa08b220ad19542c22f8132e1f622162
Rishita <burarishita@gmail.com>
2023-08-22 20:24:36 +0530

    login page to do

--- views/client/src/pages/loginSample.jsx
+++ views/client/src/pages/loginSample.jsx
@@ -1,64 +1,53 @@
 import React from 'react';
 import "../styles/login.css";
 import {
-  MDBBtn,
   MDBContainer,
-  MDBRow,
   MDBCol,
-  MDBInput
+  MDBRow,
+  MDBBtn,
+  MDBIcon,
+  MDBInput,
+  MDBCheckbox
 }
 from 'mdb-react-ui-kit';
 
 function LoginSample() {
   return (
-    <MDBContainer className="my-5 gradient-form">
+    <MDBContainer fluid className="p-3 my-5">
 
       <MDBRow>
 
-        <MDBCol col='6' className="mb-5">
-          <div className="d-flex flex-column ms-5">
-
-            <div className="text-center">
-              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                style={{width: '185px'}} alt="logo" />
-              <h4 className="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
-            </div>
-
-            <p>Please login to your account</p>
-
-
-            <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
-            <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
-
-
-            <div className="text-center pt-1 mb-5 pb-1">
-              <MDBBtn className="mb-4 w-100 gradient-custom-2">Sign in</MDBBtn>
-              <a className="text-muted" href="#!">Forgot password?</a>
-            </div>
-
-            <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
-              <p className="mb-0">Don't have an account?</p>
-              <MDBBtn outline className='mx-2' color='danger'>
-                Danger
-              </MDBBtn>
-            </div>
-
-          </div>
-
+        <MDBCol col='10' md='6'>
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone image" />
         </MDBCol>
 
-        <MDBCol col='6' className="mb-5">
-          <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4">
+        <MDBCol col='4' md='6'>
 
-            <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-              <h4 class="mb-4">We are more than just a company</h4>
-              <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </div>
 
+          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
+          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
+
+
+          <div className="d-flex justify-content-between mx-4 mb-4">
+            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+            <a href="!#">Forgot password?</a>
           </div>
+
+          <MDBBtn className="mb-4 w-100" size="lg">Sign in</MDBBtn>
+
+          <div className="divider d-flex align-items-center my-4 ">
+            <p className="text-center fw-bold mx-3 mb-0">OR</p>
+          </div>
+
+          <MDBBtn className="mb-4 w-100" size="lg" style={{backgroundColor: '#3b5998'}}>
+            <MDBIcon fab icon="facebook-f" className="mx-2"/>
+            Continue with facebook
+          </MDBBtn>
+
+          <MDBBtn className="mb-4 w-100" size="lg" style={{backgroundColor: '#55acee'}}>
+            <MDBIcon fab icon="twitter" className="mx-2"/>
+            Continue with twitter
+          </MDBBtn>
 
         </MDBCol>
 
